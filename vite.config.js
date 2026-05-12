afa84594463aed206d2e35dbe78fbf8b8ba5b048
Jian Yi <jyi.liang1@gmail.com>
2026-05-12 09:33:10 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'    // <-- ADD THIS
  
 export default defineConfig({
-  plugins: [react(), tailwindcss()],        	// <-- ADD 
- base: '/capstone/',
+  plugins: [react(), tailwindcss()],     	// <-- ADD 
 })
